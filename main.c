955173d7a7bc8aae7941806a3229b51da6086649
chandini20 <147191423+chandini20@users.noreply.github.com>
2025-01-20 19:04:49 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,21 @@
+//write a prog to swap two nums by using product , division, when an either value is not equal to zero.
+//t1: a=2 b=3 op: a=3 , b=2. 
+//if any is zero we cant take num/0 so either of num cant be zero.
+#include<stdio.h> //preprocessing
+int main(){
+    int a,b;
+    printf("enter a,b");
+    scanf("%d %d",&a,&b);
+    if(a!='0' && b!='0'){ //b!=0
+        //a=a*b; 
+        //b=a/b;
+        //a=a/b;
+        a=(a+b)-(b=a);
+        printf("a:%d b:%d",a,b);
+    }
+    else{
+        printf("enter non zero numbers only\n");
+   }
+   return 0;
+}
+
